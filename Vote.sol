b548e90d3a875b535c0253eb453902fa2aa0442c
cqyang <yangcunqiuup@163.com>
2023-04-10 22:02:14 +0800

    create

--- Vote.sol
+++ Vote.sol
@@ -16,14 +16,16 @@ struct VoteInfo {
     string title;
     // 投票主题列表
     Topic[] topics;
-    // 投票截止时间
+    // 投票截止时间 0 表示不限
     uint256 deadline;
     // 是否到期
     bool expired;
     // 投票限制
     VoteLimitEnum voteLimit;
     // 额外指定可投票的地址
-    address[] allocationAddr;
+    address[] allocationAddrs;
+    // 投票是否可用 默认不可用
+    bool available; 
 }
 
 // 主题 struct
@@ -40,6 +42,8 @@ struct Topic {
 contract Vote {
     // 所有投票列表
     VoteInfo[] public voteInfoList;
+    // 投票自增id
+    uint256 index;
     // 委托信息 mapping(VoteInfo.id => mapping(委托人 => 被委托人))
     mapping(uint256 => mapping(address => address)) public delegateMap;
     // 质押以太获取token的比值
@@ -66,6 +70,13 @@ contract Vote {
         emit BalanceLog(0, msg.sender, msg.value);
     }
 
+    // 校验谁能创建投票
+    modifier CreateVotePower() {
+        // 必须有token才能创建投票
+        require(tokenMap[msg.sender] > 0, "token is zero");
+        _;
+    }
+
     // 质押以太币, 获得token 以太:token = 1:100  
     function pledge() external payable {
         emit BalanceLog(0, msg.sender, msg.value);
@@ -73,4 +84,29 @@ contract Vote {
         tokenMap[msg.sender] += tokenCount;
         emit TokenLog(0, msg.sender, tokenCount);
     }
+
+    // 创建一个投票
+    function createVote(string memory _title, uint256 _duration, VoteLimitEnum _voteLimit, address[] memory _allocationAddrs) 
+        CreateVotePower 
+        external 
+        returns(uint256 voteId)
+    {
+        Topic[] memory ts = new Topic[](0);
+        VoteInfo memory voteInfo = VoteInfo({
+            id: index++,
+            owner: msg.sender,
+            title: _title,
+            deadline: _duration == 0 ? 0 : block.timestamp + _duration,
+            expired: false,
+            voteLimit: _voteLimit,
+            allocationAddrs: _allocationAddrs,
+            available: true,
+            topics: ts
+        });
+
+        voteInfoList.push(voteInfo);
+        return voteInfo.id;
+    }
+
+    
 }
