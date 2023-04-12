d2bc6bd2e5b2f3188d1400126e24fcb52426b9ac
cqyang <cqyang@glp.com>
2023-04-12 19:32:28 +0800

    简单投票

--- Vote.sol
+++ Vote.sol
@@ -15,25 +15,25 @@ struct VoteInfo {
     // 投票标题
     string title;
     // 投票主题列表
-    Topic[] topics;
+    mapping(uint256 => Topic) topics;
+    // 投票自增id
+    uint256 topicIndex;
+    // 投票持续时间
+    uint256 duration;
     // 投票截止时间 0 表示不限
     uint256 deadline;
-    // 是否到期
-    bool expired;
     // 投票限制
     VoteLimitEnum voteLimit;
-    // 额外指定可投票的地址
+    // 额外指定可投票的地址  voteLimit = Allocation才有用
     address[] allocationAddrs;
-    // 投票是否可用 默认不可用
-    bool available; 
+    // 投票是否开始 默认没开始
+    bool started; 
 }
 
 // 主题 struct
 struct Topic {
     // 主题名称
     string name;
-    // 如果主题是账户的话, 可以有地址
-    address addr;
     // 得票数
     uint256 voteCount;
 }
@@ -41,15 +41,21 @@ struct Topic {
 /// @title 投票平台
 contract Vote {
     // 所有投票列表
-    VoteInfo[] public voteInfoList;
+    mapping(uint256 => VoteInfo) public voteInfoMap;
     // 投票自增id
-    uint256 index;
+    uint256 voteIndex;
     // 委托信息 mapping(VoteInfo.id => mapping(委托人 => 被委托人))
     mapping(uint256 => mapping(address => address)) public delegateMap;
     // 质押以太获取token的比值
     uint8 immutable ratio;
     // 持有token
     mapping(address => uint256) public tokenMap;
+    // 记录投票地址记录
+    mapping(uint256 => mapping(address => bool)) public voteAddrMap;
+    // 记录投票主题地址记录
+    mapping(uint256 => mapping(uint256 => mapping(address => bool))) public voteTopicAddrMap;
+    // 记录地址投票主题记录
+    mapping(address => mapping(uint256 => mapping(uint256 => bool))) public addrVoteTopicMap;
 
     // 合约余额变动事件 tag: 0 收到转账, 1 向别人转出
     event BalanceLog(uint8 tag, address indexed addr, uint256 value);
@@ -77,6 +83,42 @@ contract Vote {
         _;
     }
 
+    // 校验投票是否存在
+    modifier voteExist(uint256 _voteInfoId) {
+        require(_voteInfoId <= voteIndex, "vote is not exist");
+        _;
+    }
+
+    // 校验主题是否存在
+    modifier topicExist(uint256 _voteInfoId, uint256 _topicId) {
+        require(_topicId <= voteInfoMap[_voteInfoId].topicIndex, "topic is not exist");
+        _;
+    }
+
+    // 校验谁能操作这个投票
+    modifier voteOperatePower(uint256 _voteInfoId) {
+        require(voteInfoMap[_voteInfoId].owner == msg.sender, "no operation permission");
+        _;
+    }
+
+    // 校验投票是否过期
+    modifier voteDeadline(uint256 _voteInfoId) {
+        require(voteInfoMap[_voteInfoId].deadline == 0 || block.timestamp <= voteInfoMap[_voteInfoId].deadline, "vote is expired");
+        _;
+    }
+
+    // 校验投票是否可用
+    modifier voteStarted(uint256 _voteInfoId) {
+        require(voteInfoMap[_voteInfoId].started, "vote is not start");
+        _;
+    }
+
+    // 校验投票是否不可用
+    modifier voteNotStarted(uint256 _voteInfoId) {
+        require(!voteInfoMap[_voteInfoId].started, "vote is starting");
+        _;
+    }
+
     // 质押以太币, 获得token 以太:token = 1:100  
     function pledge() external payable {
         emit BalanceLog(0, msg.sender, msg.value);
@@ -89,24 +131,92 @@ contract Vote {
     function createVote(string memory _title, uint256 _duration, VoteLimitEnum _voteLimit, address[] memory _allocationAddrs) 
         CreateVotePower 
         external 
-        returns(uint256 voteId)
-    {
-        Topic[] memory ts = new Topic[](0);
-        VoteInfo memory voteInfo = VoteInfo({
-            id: index++,
-            owner: msg.sender,
-            title: _title,
-            deadline: _duration == 0 ? 0 : block.timestamp + _duration,
-            expired: false,
-            voteLimit: _voteLimit,
-            allocationAddrs: _allocationAddrs,
-            available: true,
-            topics: ts
-        });
-
-        voteInfoList.push(voteInfo);
-        return voteInfo.id;
+        returns(uint256 voteInfoId)
+    {   
+        VoteInfo storage voteInfo = voteInfoMap[voteIndex];
+        voteInfo.id = voteIndex;
+        voteInfo.owner = msg.sender;
+        voteInfo.title = _title;
+        voteInfo.duration = _duration;
+        voteInfo.voteLimit = _voteLimit;
+        voteInfo.allocationAddrs = _allocationAddrs;
+        voteInfo.started = false;
+        return voteIndex++;
     }
 
-    
+    // 为投票添加主题
+    function addTopicList(uint256 _voteInfoId, string[] memory _nameList) 
+        voteExist(_voteInfoId) 
+        voteOperatePower(_voteInfoId)
+        voteNotStarted(_voteInfoId)
+        external 
+     {
+        VoteInfo storage voteInfo = voteInfoMap[_voteInfoId];
+        for (uint256 i = 0; i < _nameList.length; i++) {
+            Topic memory t = Topic({
+                name: _nameList[i],
+                voteCount: 0
+            });
+
+            voteInfo.topics[voteInfo.topicIndex] = t;
+            voteInfo.topicIndex++;
+        }
+    }
+
+    // 开始投票
+    function startVote(uint256 _voteInfoId) voteExist(_voteInfoId) voteOperatePower(_voteInfoId) voteDeadline(_voteInfoId) public {
+        VoteInfo storage voteInfo = voteInfoMap[_voteInfoId];
+        // 是否已经开始
+        require(!voteInfo.started, "vote is starting");
+        // 是否有主题
+        require(voteInfo.topicIndex > 0, "vote not topic");
+        voteInfo.deadline = voteInfo.duration == 0 ? 0 : block.timestamp + voteInfo.duration;
+        voteInfo.started = true;
+    }
+
+    // 投票
+    function vote(uint256 _voteInfoId, uint256 _topicId) 
+        voteExist(_voteInfoId) 
+        topicExist(_voteInfoId, _topicId) 
+        voteDeadline(_voteInfoId)
+        voteStarted(_voteInfoId)
+        external 
+    {
+        // 校验是否已经投过票 只能投一次 
+        require(!voteAddrMap[_voteInfoId][msg.sender], "address is voted");
+        if (voteInfoMap[_voteInfoId].voteLimit == VoteLimitEnum.Token) {
+            // 有token才能投票
+            require(tokenMap[msg.sender] > 0, "token is zero");
+        } else if (voteInfoMap[_voteInfoId].voteLimit == VoteLimitEnum.Allocation) {
+            // 额外指定地址
+            require(containAddr(msg.sender, voteInfoMap[_voteInfoId].allocationAddrs), "not allocation address");
+        }
+
+        // 记录
+        voteAddrMap[_voteInfoId][msg.sender] = true;
+        
+        // 票数+1
+        voteInfoMap[_voteInfoId].topics[_topicId].voteCount++;
+        // 投票记录
+        voteTopicAddrMap[_voteInfoId][_topicId][msg.sender] = true;
+        addrVoteTopicMap[msg.sender][_voteInfoId][_topicId] = true;
+    }
+
+    function containAddr(address addr, address[] memory addrList) private pure returns(bool isContain) {
+        for (uint256 i = 0; i < addrList.length; i++) {
+            if (addr == addrList[i]) {
+                return true;
+            }
+        }
+    }
+
+    // 获取投票主题
+    function getTopicListByVoteId(uint256 _voteInfoId) voteExist(_voteInfoId) external view returns(Topic[] memory) {
+        VoteInfo storage voteInfo = voteInfoMap[_voteInfoId];
+        Topic[] memory topicArr = new Topic[](voteInfo.topicIndex);
+        for (uint256 i = 0; i < voteInfo.topicIndex; i++) {
+            topicArr[i] = voteInfo.topics[i];
+        }
+        return topicArr;
+    }
 }
